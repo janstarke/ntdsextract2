a69a772bb396e49bea42a31eb90f61388db8b841
Jan Starke <jan.starke@posteo.de>
2024-08-28 19:19:05 +0200

    use configurable datetime format

--- tests/test_plain.rs
+++ tests/test_plain.rs
@@ -1,35 +1,38 @@
 use std::{
+    collections::HashMap,
     io::{BufReader, Cursor},
-    path::PathBuf, collections::HashMap,
+    path::PathBuf,
 };
 
 use assert_cmd::Command;
 use libntdsextract2::{ntds::Person, CsvSerialization};
-
 
 #[test]
 fn test_plain() {
     let dstfile = get_test_data("ntds_plain.dit");
 
     let mut cmd = Command::cargo_bin("ntdsextract2").unwrap();
-    let result = cmd.arg(dstfile).arg("user").ok();
+    let result = cmd
+        .arg(dstfile)
+        .arg("user")
+        .ok();
 
     match &result {
         Ok(out) => {
             let mut users = HashMap::new();
             let reader = BufReader::new(Cursor::new(&out.stdout));
             let mut rdr = csv::Reader::from_reader(reader);
-            
+
             for result in rdr.deserialize() {
                 let record: Person<CsvSerialization> = result.unwrap();
                 users.insert(record.sam_account_name().as_ref().unwrap().clone(), record);
             }
 
             assert!(users.contains_key("Administrator"));
-            assert!(! users.contains_key("InvalidUser"));
+            assert!(!users.contains_key("InvalidUser"));
 
             let admin = users.get("Administrator").unwrap();
-            assert!(! admin.is_deleted())
+            assert!(!admin.is_deleted())
         }
         Err(why) => {
             println!("{why}");
