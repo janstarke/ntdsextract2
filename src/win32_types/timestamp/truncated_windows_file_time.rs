b633755abca9490476baacd177fa783509d4a409
Jan Starke <jan.starke@posteo.de>
2025-05-09 13:41:02 +0200

    use correct timestamp calculation

--- src/win32_types/timestamp/truncated_windows_file_time.rs
+++ src/win32_types/timestamp/truncated_windows_file_time.rs
@@ -1,4 +1,5 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Duration, Utc};
+use lazy_static::lazy_static;
 use libesedb::systemtime_from_filetime;
 
 use crate::impl_timestamp;
@@ -8,9 +9,13 @@ pub struct TruncatedWindowsFileTime(DateTime<Utc>);
 
 impl_timestamp!(TruncatedWindowsFileTime);
 
+lazy_static!{
+    static ref BASE_TIME: DateTime<Utc> = systemtime_from_filetime(0).into();
+}
+
 impl From<u64> for TruncatedWindowsFileTime {
     fn from(value: u64) -> Self {
-        Self(systemtime_from_filetime(value*10_000).into())
+        Self(*BASE_TIME + Duration::seconds(value.try_into().unwrap()))
     }
 }
 impl From<DateTime<Utc>> for TruncatedWindowsFileTime {
@@ -21,5 +26,17 @@ impl From<DateTime<Utc>> for TruncatedWindowsFileTime {
 impl From<TruncatedWindowsFileTime> for DateTime<Utc> {
     fn from(value: TruncatedWindowsFileTime) -> Self {
         value.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::win32_types::TruncatedWindowsFileTime;
+
+    #[test]
+    fn convert_from_ntds() {
+        let ds_record_time = 13390472401u64;
+        let ts = TruncatedWindowsFileTime::from(ds_record_time);
+        assert_eq!(ts.0.to_rfc3339(), "2025-04-30T07:40:01+00:00");
     }
 }
